61c3e7c5604d4e614c2dea29fd4b522c0dcfcb24
dtovrea <50924646+dtovrea@users.noreply.github.com>
2019-05-22 11:12:54 -0700

    Add files via upload

---
+++ ct-alternative/en.dict.ct-alternative.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+'Menu:Alternatives' => 'Alternatives',
+'Class:Alternative/Attribute:name' => 'Name',
+'Class:Alternative/Attribute:org_id' => 'Organization',
+'Class:Alternative/Attribute:redundancyType' => 'Redundancy',
+'Class:Alternative/Attribute:dependentOn_list' => 'Redundancy list',
+'Class:Alternative/Attribute:redundancyType/disabled' => 'The alternative is satisfied if all the supporting objects are up~~',
+'Class:Alternative/Attribute:redundancyType/count' => 'The  alternative is satisfied if at least %1$s supporting objects is(are) up~~',
+'Class:Alternative/Attribute:redundancyType/percent' => 'The alternative is satisfied if at least %1$s %% of the supporting objects are up~~',
+'Class:Alternative/Attribute:dependent_list' => 'Alternative is used by',
+'Class:Alternative/Attribute:component_list' => 'CIs that make up this alternative',
+'Class:Alternative/Attribute:automanual' => 'Automatic/manual selection',
+'Class:FunctionalCI/Attribute:alternative_list' => 'Used in alternative(s)',
+'Class:FunctionalCI/Attribute:requiredalternatives_list' => 'Required alternative(s)',
+));
+?>
+
+
